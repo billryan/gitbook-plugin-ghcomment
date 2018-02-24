987f2f3287246e303e51258a4809bbd10556c104
billryan <yuanbin2014@gmail.com>
2018-02-24 23:14:58 +0800

    bump version 0.1.0
    	new file:   LICENSE
    	new file:   README.md
    	new file:   assets/plugin.css
    	new file:   index.js
    	new file:   package.json

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = {
+  book: {
+      assets: "./assets",
+      js: [
+          "https://unpkg.com/github-comments@0.5.0/gc.js"
+      ],
+      css: [
+          "plugin.css"
+      ]
+  },
+  hooks: {
+    "page": function(page) {
+      if (this.output.name != 'website') return page;
+      if (!page.issue_id) return page;
+
+      var _repo = this.config.get('pluginsConfig.ghcomment.repo');
+      var _owner = this.config.get('pluginsConfig.ghcomment.owner');
+      var _issue_id = page.issue_id;
+
+      //var _gh_comments_js = '<script async src="https://unpkg.com/github-comments/gc.js"></script>'
+      var _gh_comments_div = '<div class="gc-comments" data-repos="_owner/_repo" data-issues="_issue_id" >'.replace('_owner', _owner).replace('_repo', _repo).replace('_issue_id', _issue_id);
+      var _gh_comments_div = _gh_comments_div + '<div class="gc-comments-title">评论</div>';
+      var _gh_comments_div = _gh_comments_div + '<div class="gc-comments-info">';
+      var _gh_comments_div = _gh_comments_div + '请访问 <a target="_blank" href="issues_link">GitHub issue</a> 提交评论';
+      var _gh_comments_div = _gh_comments_div + '</div></div>';
+      page.content = page.content + _gh_comments_div;
+      return page;
+    }
+  }
+};
